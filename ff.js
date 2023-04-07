c92c4f083a81bb65329de23ceb869183548e414c
Kellyh910 <130156792+Kellyh910@users.noreply.github.com>
2023-04-07 14:38:24 +0100

    Add files via upload

---
+++ ff.js
@@ -0,0 +1,4 @@
+const button = document.querySelector("button");
+button.addEventListener("click", e => {
+  document.querySelector(".element").classList.toggle("flip-scale-down-diag-2")
+})// JavaScript Document
